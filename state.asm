165d9c9c5fe43185e22853f0df3d8ffef03283f0
AtriaX <909445700@qq.com>
2023-12-19 17:11:19 +0800

    添加麦克风模块

--- state.asm
+++ state.asm
@@ -21,7 +21,7 @@ include     wdm.inc
 extern $h_instance:dword ; 程序实例句柄
 extern $h_window_main:dword ; 主窗口句柄
 extern $state:GameState ; 游戏状态
-
+extern $voice_input:dword ; 麦克风模块：是否到达阈值
 
 _get_image PROTO @id:DWORD ; 获取图像
 _check_obj_in_window PROTO @p_obj:DWORD ; 检查对象是否在窗口内
@@ -43,7 +43,8 @@ a_render_object_test RenderObject 10 dup (<>) ; 渲染对象数组
 x_text dword 0
 
 $state_jumping  dword 0;鹅是否滞空
-$state_keypress  word 0;上一次检测到的按键输入(word->SHORT)
+;$state_keypress  word 0;上一次检测到的按键输入(word->SHORT)
+$state_voiceinput  dword 0;上一次检测到的按键输入(word->SHORT)
 
 
 FLOATSCONST1 REAL4 0.001
@@ -82,7 +83,8 @@ _init_state PROC uses ecx
 	local @Bkg2:RenderObject
 
 	mov $state_jumping,0
-	mov $state_keypress,0
+	; mov $state_keypress,0
+	mov $state_voiceinput,0
 	mov $state.score, 0
 	mov $state.status,GAME_STATUS_INIT
 	; 加载 global vx
@@ -319,10 +321,10 @@ _state_update ENDP
 
 
 _check_key_down PROC uses ecx edi edx
-	local @keystate:word
-	invoke GetAsyncKeyState, VK_SPACE
-	mov  @keystate,ax
-	.if eax != 0
+	; local @keystate:word
+	; invoke GetAsyncKeyState, VK_SPACE
+	; mov  @keystate,ax
+	.if $voice_input != 0
 		;DEBUG
 		;szText debugstr1,"detect key pressed"
 		;invoke crt_printf,addr debugstr1
@@ -363,7 +365,8 @@ _check_key_down PROC uses ecx edi edx
 		.endif
 
 		.if $state.status == GAME_STATUS_OVER
-			.if $state_keypress == 0
+			; .if $state_keypress == 0
+			.if $state_voiceinput == 0
 				invoke _reset_state
 			.endif
 		.endif
@@ -374,9 +377,10 @@ _check_key_down PROC uses ecx edi edx
 	; 	invoke crt_printf,addr debugstr2
 	.endif
 
-
-	mov cx,@keystate
-	mov $state_keypress,cx
+	; mov cx, @keystate
+	; mov $state_keypress,cx
+	mov ecx, $voice_input
+	mov $state_voiceinput,ecx
 
 	ret
 
@@ -600,7 +604,8 @@ _update_goose PROC uses esi ecx @p_goose:DWORD
 		fstp @fdeltat
 
 		; 加载加速度
-		movzx eax,$state_keypress
+		; movzx eax,$state_keypress
+		mov eax,$state_voiceinput
 		.if eax != 0
 			mov eax,G
 			sub eax,JUMPA
